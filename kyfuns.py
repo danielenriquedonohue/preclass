9ba7c911754cece89bcba4c1c26eae0ad2c97fa7
Kyle <626100+kbiondo@users.noreply.github.com>
2022-11-01 13:36:54 -0700

    writing comments as I go

--- kyfuns.py
+++ kyfuns.py
@@ -1,1 +1,11 @@
 #link to learning https://www.udemy.com/course/pythonforbeginnersintro/learn/lecture/8256980#overview
+
+# #4 Variables and multiple assignment
+print('hello world')
+
+import sys; #import the functions that tie into the operating system
+
+# print the system install imformation 
+print('Print %s on %s' % (sys.version, sys.platform))
+#this print fucntion passes uses placeholders to pass in the information from the installed python version and the installed python platform. 
+# The % sign after the tells the print function
